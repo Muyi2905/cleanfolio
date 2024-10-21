090964277eecb81031b50a6e5053cc820350b441
Muyi2905 <obaremimuyiwa@gmail.com>
2024-10-21 11:00:12 +0100

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/Muyi2905/portfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+     Muyiwa Obaremi
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  homepage: 'https://rjshkhr.github.io/Muyi2905',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Muyiwa Obaremi',
+  role: 'Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'Passionate about building robust backend solutions and continuously expanding my expertise in DevOps practices, with a touch of frontend development. I specialize in designing and developing scalable, efficient, and reliable backend systems while implementing automation and cloud infrastructure. While my primary focus is backend and DevOps, I also enjoy working on frontend technologies to create seamless, user-friendly experiences. I`m always eager to tackle complex challenges and stay ahead of emerging trends in the tech industry.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/muyiwaobaremi/',
+    github: 'https://github.com/Muyi2905',
   },
 }
 
@@ -47,25 +47,25 @@ const projects = [
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
+  'Golang',
+  'Python',
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Docker',
+  'Postgres',
+  'Redis',
+  'Nodejs',
+  'Expressjs',
+  'MongoDb',
+  'Mysql',
+
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'obaremimuyiwa@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
